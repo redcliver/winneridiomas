4047dd12acdedf51d8ce1e62a40bb40bf531d085
igor <igor-peres@hotmail.com>
2019-08-13 01:00:18 -0400

    1.0

--- winneridiomas/wsgi.py
+++ winneridiomas/wsgi.py
@@ -12,7 +12,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sistemas.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "winneridiomas.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
