77d9ec0d0d574e2ee971cb4a772f93f6bd6244e0
igor <igor-peres@hotmail.com>
2019-08-12 22:49:49 -0400

    1.0

--- winneridiomas/settings.py
+++ winneridiomas/settings.py
@@ -128,5 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
