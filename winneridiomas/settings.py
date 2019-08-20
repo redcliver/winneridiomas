b3412eb422d4dc486ae1f084a40560fe92aaa3f9
igor <igor-peres@hotmail.com>
2019-08-19 23:56:27 -0400

    ADicionado a tela de erro costumizada como padrão

--- winneridiomas/settings.py
+++ winneridiomas/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(6$+3na=rp4n_m$i-gwee+yve4y-kn5+bv(!-gnxti8!59v)t!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
