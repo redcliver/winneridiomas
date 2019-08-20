5819964eea5f21c463c4b05aa85e137b61115c1b
redcliver <igor-peres@hotmail.com>
2019-08-20 13:47:24 -0300

    1.1
    Começado o endereçamento da pagina principal.

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 from . import views
-from django.urls import path
 
 
 urlpatterns = [
     url(r'^$', views.paginaPrincipal),
     url(r'^cursos', views.cursos),
+    url(r'^unidades', views.cursos),
+    url(r'^nivelamento', views.cursos),
+    url(r'^contato', views.cursos),
+    url(r'^entrar', views.cursos),
+    url(r'^instituicao', views.cursos),
+    url(r'^metodo', views.cursos),
+    url(r'^colaboradores', views.cursos),
 ]
 
