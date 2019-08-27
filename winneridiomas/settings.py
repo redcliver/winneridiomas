1f274c5e1c0971784b99b54238ead966e8ff8b70
igor <igor-peres@hotmail.com>
2019-08-26 21:02:14 -0400

    1.0

--- winneridiomas/settings.py
+++ winneridiomas/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import posixpath
-#import dj_database_url
-#import django_heroku
+import dj_database_url
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -94,7 +94,7 @@ DATABASES = {
     }
 }
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -140,4 +140,4 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
