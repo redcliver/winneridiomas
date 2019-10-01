a26590ef5f98a9dca8021dd0e055ff098d6ef470
RedCliver <igor-peres@hotmail.com>
2019-10-01 00:01:59 -0300

    Finalizado o redirecionamento para o email do cliente

--- winneridiomas/settings.py
+++ winneridiomas/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'winneridiomas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
