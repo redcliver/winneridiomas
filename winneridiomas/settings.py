d2fe21912f96dd097f302b09dedcc1ee422db4ba
RedCliver <igor-peres@hotmail.com>
2019-09-30 23:58:16 -0300

    Finalizado o redirecionamento para o email do cliente

--- winneridiomas/settings.py
+++ winneridiomas/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import posixpath
-#import dj_database_url
-#import django_heroku
+import dj_database_url
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -30,7 +30,7 @@ SECRET_KEY = '(6$+3na=rp4n_m$i-gwee+yve4y-kn5+bv(!-gnxti8!59v)t!'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
+ #   'localhost',
     'winneridiomas.herokuapp.com',
 ]
 
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'winneridiomas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -146,4 +146,4 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
