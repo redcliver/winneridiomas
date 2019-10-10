c869631c240cabda847364a28b63ba8b6150dfb1
RedCliver <igor-peres@hotmail.com>
2019-10-10 00:50:03 -0300

    ajustes tela gerencia mobile

--- winneridiomas/settings.py
+++ winneridiomas/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import posixpath
-#import dj_database_url
-#import django_heroku
+import dj_database_url
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -89,18 +89,25 @@ WSGI_APPLICATION = 'winneridiomas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'winneridiomas',
-         'USER': 'postgres',
-         'PASSWORD': '957855',
-         'HOST': '127.0.0.1',
-         'PORT': '', # 8000 is default
-     }
- }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'winneridiomas',
+#         'USER': 'postgres',
+#         'PASSWORD': '957855',
+#         'HOST': '127.0.0.1',
+#         'PORT': '', # 8000 is default
+#     }
+# }
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -155,4 +162,4 @@ LOGIN_REDIRECT_URL = '/entrar'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
